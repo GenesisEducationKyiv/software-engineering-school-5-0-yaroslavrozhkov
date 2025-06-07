699b03c30b0b0701d0f69b2e8e552ac7557ac9b2
OFFICEKIEV\y.rozhkov <gitlab.ci@temabit.com>
2025-06-07 06:39:52 +0300

    feat: extend eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,30 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
   {
-    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    ignores: ['eslint.config.mjs', 'public/**'],
+  },
+  {
+      ignores: ['eslint.config.mjs', 'public/**'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+    },
+  },
+  {
+    name: 'prettier-override',
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: prettierConfig.rules,
   }
 );
